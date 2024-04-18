47a98b7eff0457c5068753f211e0e59f31cb769c
ali <165990962+Persian01@users.noreply.github.com>
2024-04-18 10:44:47 +0330

    calculator
    This is a Python calculator file

---
+++ calculator.py
@@ -0,0 +1,21 @@
+number1 = int(input("enter number1 :"))
+ol = input("enter")
+number2 = int(input("enter number2 :"))
+if ol == "+":
+    h = number1 + number2
+    print(h)
+elif ol == "-":
+    a = number1 - number2
+    print(a)
+elif ol == "*":
+    s = number1 * number2
+    print(s)
+elif ol == "/":
+    d = number1 / number2
+    print(d)
+elif ol == "**":
+    g = number1 ** number2
+    print(g)
+elif ol == "//":
+    p = number1 // number2
+    print(p)
